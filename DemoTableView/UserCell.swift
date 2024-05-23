7a2f5b47b55bced034b757eb5f11bac0d34ca0fe
papasmf1 <papasmf1@gmail.com>
2024-05-23 14:20:59 +0900

    이미지뷰까지 완료

---
+++ DemoTableView/UserCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserCell.swift
+//  DemoTableView
+//
+//  Created by user on 5/23/24.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
